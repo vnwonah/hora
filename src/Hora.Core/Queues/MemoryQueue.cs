95604a2d48d389f52c7d0c3e2b87a8199c6d4640
Vincent Nwonah <vnwonah@outlook.com>
2020-05-07 16:55:52 +0100

    removed static constructor

--- src/Hora.Core/Queues/MemoryQueue.cs
+++ src/Hora.Core/Queues/MemoryQueue.cs
@@ -5,15 +5,12 @@ namespace Hora.Core.Queues
 {
     public static class MemoryQueue
     {
-        static MemoryQueue()
-        {
-            _queue = new List<object>();
-        }
+       
         private static int _currentIndex;
         private static int _lastIndex;
         private static bool _processing;
 
-        private static List<object> _queue;
+        private static List<object> _queue = new List<object>();
 
         public static void Enqueue<T>(Func<T> p)
         {
